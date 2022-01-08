f15f9ee11579e67de8908cfbd6d50dcf05cde49d
taxicat1 <x14fgg@gmail.com>
2022-01-08 14:06:54 -0500

    General cleanup

--- poly1305.c
+++ poly1305.c
@@ -5,12 +5,12 @@
 #include <gmp.h>
 
 void poly1305_clamp(unsigned char r[16]) {
-  r[3] &= 15;
-  r[7] &= 15;
+  r[3]  &= 15;
+  r[7]  &= 15;
   r[11] &= 15;
   r[15] &= 15;
-  r[4] &= 252;
-  r[8] &= 252;
+  r[4]  &= 252;
+  r[8]  &= 252;
   r[12] &= 252;
 }
 
@@ -24,29 +24,29 @@ void poly1305_mac(uint8_t key[32], char* plaintext,
   // as little-endian 16-byte words.
   // Here (r, s) = key, where we ensure some properties about r just in case,
   // via clamping.
-
+  
   memset(tag, 0, 16);
   mpz_t r, s;
   mpz_inits(r, s, NULL);
-
+  
   // We must clamp r before loading it as a number.
   poly1305_clamp(key); 
   mpz_import(r, 1, 1, 16, -1, 0, key); 
   mpz_import(s, 1, 1, 16, -1, 0, key + 16); 
-
+  
   // The RFC has a typo and calls this `accumulator` here, and
   // nowhere else.
   mpz_t a;
   mpz_init(a);
-
+  
   // p = 2^130 - 5.
   mpz_t p, one;
   mpz_inits(p, one, NULL);
   mpz_set_ui(one, 1);
   mpz_mul_2exp(p, one, 130);
   mpz_sub_ui(p, p, 5);
-
-
+  
+  
   mpz_t n, nadd;
   mpz_inits(n, nadd, NULL);
   for (int i = 0; i < plaintext_length; i += 16) {
@@ -54,22 +54,22 @@ void poly1305_mac(uint8_t key[32], char* plaintext,
     int j = i + 16;
     if (j > plaintext_length) j = plaintext_length;
     j -= i;
-
+    
     mpz_import(n, 1, 1, j, -1, 0, plaintext + i); 
-
+    
     // We must add 2^(8 * j) to n, before adding n to a.
     mpz_mul_2exp(nadd, one, 8 * j);
     mpz_add(a, a, nadd);
-
+    
     // a := ((a + n) * r) % p .
     mpz_add(a, a, n);
     mpz_mul(a, a, r);
     mpz_mod(a, a, p);
   }
-
+  
   mpz_add(a, a, s);
-
+  
   mpz_export(tag, NULL, -1, 1, 0, 0, a);
- 
+  
   mpz_clears(a, r, s, p, one, n, nadd, NULL);
 }
--- poly1305_demo.c
+++ poly1305_demo.c
@@ -1,17 +1,17 @@
 #include "poly1305.h"
 #include <stdio.h>
 
-int main() {
+int main(void) {
   unsigned char key[32] = {0x85, 0xd6, 0xbe, 0x78, 0x57, 0x55, 0x6d, 0x33,
                            0x7f, 0x44, 0x52, 0xfe, 0x42, 0xd5, 0x06, 0xa8,
                            0x01, 0x03, 0x80, 0x8a, 0xfb, 0x0d, 0xb2, 0xfd,
                            0x4a, 0xbf, 0xf6, 0xaf, 0x41, 0x49, 0xf5, 0x1b};
   char plaintext[34] = "Cryptographic Forum Research Group";
   size_t plaintext_length = 34;
-
+  
   unsigned char tag[16];
   poly1305_mac(key, plaintext, plaintext_length,  tag);
-
+  
   for (int i = 0; i < 16; ++i) {
     if (i > 0) printf(":");
     printf("%02x", tag[i]);
